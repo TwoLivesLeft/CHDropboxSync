502a221ff482ae05a22d05c2923ec7a72e8b42c0
Simeon <simeon@twolivesleft.com>
2012-04-14 14:50:35 +0930

    Modified to allow optional showing of alerts, delegate is notified with (optional) progress updates. Added support for multiple accounts through clearLastSyncData method, allow setting of different root directory, ability to cancel mid-sync.

--- CHDropboxSync.h
+++ CHDropboxSync.h
@@ -4,14 +4,37 @@
 //  Created by Chris Hulbert on 11/12/11.
 //  Copyright (c) 2011 Splinter Software. All rights reserved.
 //  MIT license - no warranties!
+//
+//  Modified by Simeon Nasilowski 14/04/12
+//  Additional syncing detail, cancel, multiple account support 
+//  Ability to toggle alerts
 
 #import <Foundation/Foundation.h>
 #import "DropboxSDK.h"
 
+@class CHDropboxSync;
+
+@protocol CHDropboxSyncDelegate <NSObject>
+
+- (void) syncStarted:(CHDropboxSync*)sync;
+- (void) sync:(CHDropboxSync*)sync remainingTasks:(NSInteger)remaining;
+- (void) sync:(CHDropboxSync*)sync file:(NSString*)path progress:(CGFloat)progress;
+- (void) syncComplete:(CHDropboxSync*)sync;
+- (void) syncCancelled:(CHDropboxSync*)sync;
+- (void) syncFailed:(CHDropboxSync*)sync;
+
+@end
+
 @interface CHDropboxSync : NSObject<DBRestClientDelegate, UIAlertViewDelegate>
 
-@property(assign) id delegate;
+@property(assign) id<CHDropboxSyncDelegate> delegate;
+@property(assign) BOOL showAlerts;
+@property(retain,nonatomic) NSString *rootDirectory;
 
 - (void)doSync;
+- (void)doSyncWithAlert;
+- (void)cancel;
+
++ (void)clearLastSyncData;
 
 @end
